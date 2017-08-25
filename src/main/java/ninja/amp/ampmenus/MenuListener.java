8cad14bb3bb6211c467724e02b2b4cf9b37d3360
Jp78 <mcjp78@users.noreply.github.com>
2017-08-24 21:26:55 -0700

    Update MenuListener.java

--- src/main/java/ninja/amp/ampmenus/MenuListener.java
+++ src/main/java/ninja/amp/ampmenus/MenuListener.java
@@ -31,6 +31,8 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredListener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+
 
 /**
  * Passes inventory click events to their menus for handling.
